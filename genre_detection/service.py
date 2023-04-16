cd854237506c9ecf79be3aa20263cc9d0821109c
MaxM1211 <maximaes12@gmail.com>
2023-04-16 13:24:36 +0200

    Add basic service structure

--- genre_detection/service.py
+++ genre_detection/service.py
@@ -1,16 +1,29 @@
+import os
+
+import numpy as np
+
 from easymlserve import EasyMLServer, EasyMLService
 
-
-class BasicCSVService(EasyMLService):
-    """Just a dummy service that only returns empty dict."""
-
-    def process(self, df):
-        return {}
+from api_schema import APIRequest, APIResponse
 
 
-service = BasicCSVService()
-server = EasyMLServer(service)
+class HistogramImageService(EasyMLService):
+    """Histogram calculation example service."""
+
+    def load_model(self):
+        model_path = os.path.join('model', 'model.xml')
+        #TODO Save to self.model
+
+    def process(self, request: APIRequest) -> APIResponse:
+        """ Process REST API request and return genre."""
+        #TODO get genre
+        #detections = self.model.detectGenre(request)
+        
+        response = {'genre': ['metal']}
+        print(response)
+        return response
 
 
-if __name__ == '__main__':
-    server.deploy()
+service = HistogramImageService()
+server = EasyMLServer(service, uvicorn_args={'host': '0.0.0.0', 'port': 8000})
+server.deploy()
