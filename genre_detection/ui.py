a17c1249851c8403e3ad570ec610e71e0d6c08cb
schu21de <93709656+schu21de@users.noreply.github.com>
2023-04-21 12:57:16 +0200

    added preprocess method and first model

--- genre_detection/ui.py
+++ genre_detection/ui.py
@@ -1,5 +1,8 @@
+import librosa as librosa
+
 from easymlserve.ui import GradioEasyMLUI, QtEasyMLUI
 from easymlserve.ui.type import *
+from sklearn.preprocessing import StandardScaler
 
 from api_schema import *
 
@@ -9,8 +12,9 @@ class BeatBotUI(GradioEasyMLUI):
     This UI accepts any music file, process it and shows the genre of the music
     """
 
-    def prepare_request(self, path: str) -> APIRequest:
-        print(path)
+    def prepare_request(self, file: str) -> APIRequest:
+        print(file)
+        self.preprocess_music(file)
         return {"music_array": "test"}
 
     def process_response(self, request: APIRequest, response: APIResponse) -> Plot:
@@ -21,6 +25,28 @@ class BeatBotUI(GradioEasyMLUI):
             path_to_img = "assets/genres/" + genre + ".png"
         
         return genre, path_to_img
+
+    def preprocess_music(self, songname: str):
+        y, sr = librosa.load(songname, mono=True, duration=3)
+        chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+        rmse = librosa.feature.rms(y=y)
+        spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+        spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+        rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+        zcr = librosa.feature.zero_crossing_rate(y)
+        mfcc = librosa.feature.mfcc(y=y, sr=sr)
+        array = [np.mean(chroma_stft), np.mean(rmse), np.mean(spec_cent), np.mean(spec_bw), np.mean(rolloff), np.mean(zcr)]
+
+        for e in mfcc:
+             array.append(np.mean(e))
+
+        np_array = np.array([array])
+        print(np_array)
+        print(np_array.dtype)
+        scaler = StandardScaler()
+        np_array = scaler.fit_transform(np_array)
+        print(np_array)
+        return np_array
 
 
 if __name__ == "__main__":
